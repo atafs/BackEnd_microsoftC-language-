3b0b641d5ca7480c7556cf4867d273d7892fe074
atafs <atafs@iscte-iul.pt>
2015-09-24 19:21:53 +0100

    first commit

---
+++ americoLIB_001_helloWorld/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    //WINDOW
+    public partial class Form1 : Form
+    {
+        //START
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //PRINT MESSAGE
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //WINDOW
+            MessageBox.Show("HELLO WORLD OF C#");
+        }
+    }
+}
